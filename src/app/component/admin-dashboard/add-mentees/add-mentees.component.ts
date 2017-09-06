bb73f87d4e9b017bce521dbf6a3abd5591815d53
Manuel Yépez <manuel.yepez@gft.com>
2017-09-06 16:39:58 -0600

    Mentee typeahead working with filter.

--- src/app/component/admin-dashboard/add-mentees/add-mentees.component.ts
+++ src/app/component/admin-dashboard/add-mentees/add-mentees.component.ts
@@ -1,6 +1,14 @@
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+
 import {Component, Input, OnInit} from '@angular/core';
 import {Employee} from '../../../model/employee/employee';
 import {EmployeeService} from '../../../service/employee.service';
+import {environment} from '../../../../environments/environment';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-add-mentees',
@@ -13,6 +21,23 @@ export class AddMenteesComponent implements OnInit {
   public mentor: Employee;
 
   mentees: Employee[];
+
+  // Behavior for the typeahead: Triggers after 200ms, after 3 letters and waits for changes on the input.
+  searchTerm = (text$: Observable<string>) =>
+    text$
+      .debounceTime(environment.typeaheadDebounceTime)
+      .distinctUntilChanged()
+      .switchMap(term => term.length < environment.typeaheadActivationChars ? [] : this.employeeService.getAllEmployees(term)
+        .map(employees => this.employeeFilter(employees)));
+
+  // Formats the selected employee in the input
+  formatter = (x: { firstName: string, lastName: string }) =>
+    x.firstName + ' ' + x.lastName;
+
+  // If the employee is already on the list of mentees or is the mentor himself, remove it from the typeahead results.
+  private employeeFilter = (employees) =>
+    employees.filter(e => this.mentees
+      .map(mentee => mentee.id).indexOf(e.id) < 0 && this.mentor.id !== e.id);
 
   constructor(
     private employeeService: EmployeeService
